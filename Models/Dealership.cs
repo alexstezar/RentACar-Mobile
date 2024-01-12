130d0e221aff9d8935efab04ee5b8f587261f8ce
Alex Stezar <alex.stezar07@gmail.com>
2024-01-12 18:22:25 +0200

    second commit

---
+++ Models/Dealership.cs
@@ -0,0 +1,23 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLiteNetExtensions.Attributes;
+
+namespace RentACar_Mobile_.Models
+{
+    public class Dealership
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public string Details { get { return Name + " " + Adress; } }
+
+        [OneToMany]
+        public List<Car> Cars { get; set; }
+
+    }
+}
